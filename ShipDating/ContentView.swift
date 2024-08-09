3810567588998ec55395fa7ee9a72bc42dda32e5
Rongtian Yue <ron@yueventures.com>
2024-08-08 19:15:28 -0700

    init

---
+++ ShipDating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShipDating
+//
+//  Created by Rongtian Yue on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShipDating/ShipDatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipDatingApp.swift
+//  ShipDating
+//
+//  Created by Rongtian Yue on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ShipDatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
